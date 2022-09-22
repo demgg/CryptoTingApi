0ca37047fbfe7446bb9e9f08779940c31110d2bb
KD <demiebroerswerk@gmail.com>
2022-09-23 01:21:10 +0200

    for school

---
+++ AccountAPI/Controllers/AssetsController.cs
@@ -0,0 +1,33 @@
+﻿using AccountAPI.Models.CoinApi;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace AccountAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssetsController : ControllerBase
+    {
+        private static string API_KEY = "74f91c68-9e76-4333-8a3a-d45cdd0c2ea8";
+
+        [HttpGet("GetPeepo")]
+        public async Task<List<Coin>> GetPeepo()
+        {
+            var client = new RestClient("https://rest.coinapi.io/v1/assets");
+            var request = new RestRequest();
+            request.Method = Method.Get;
+            request.AddHeader("X-CoinAPI-Key", "D06A2E51-68CF-462C-83D2-C63D4EB149D6");
+
+            RestResponse response = client.Execute(request);
+            var coin = JsonConvert.DeserializeObject<List<Coin>>(response.Content);
+ 
+
+            return coin;
+        }
+    }
+}
---
+++ AccountAPI/Models/CoinApi/Coin.cs
@@ -0,0 +1,24 @@
+﻿namespace AccountAPI.Models.CoinApi
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Coin
+    {
+        public string AssetId { get; set; }
+        public string Name { get; set; }
+        public int TypeIsCrypto { get; set; }
+        public string DataStart { get; set; }
+        public string DataEnd { get; set; }
+        public DateTime DataQuoteStart { get; set; }
+        public DateTime DataQuoteEnd { get; set; }
+        public DateTime DataOrderbookStart { get; set; }
+        public DateTime DataOrderbookEnd { get; set; }
+        public DateTime DataTradeStart { get; set; }
+        public DateTime DataTradeEnd { get; set; }
+        public int DataSymbolsCount { get; set; }
+        public double Volume1hrsUsd { get; set; }
+        public double Volume1dayUsd { get; set; }
+        public double Volume1mthUsd { get; set; }
+        public double PriceUsd { get; set; }
+    }
+
+}
